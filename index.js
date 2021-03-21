32d3c9ff2459cef510ec89537089b4660168d432
David <dycchang@gmail.com>
2021-03-21 14:24:48 -0400

    Done with assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(n1,n2) {
+    return n1 + n2;
+  }
+function subtract(n1,n2) {
+    return n1 - n2;
+}
+function divide(n1,n2) {
+    return n1 / n2 ; 
+}
+function multiply(n1,n2) {
+    return n1 * n2 ; 
+}
+function increment(n){
+    n++;
+    return n; 
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
